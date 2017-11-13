9adee8c76e73add26f5f7353cefb2b8203d422c9
Chandrakumar Vijayakumar <s2gt.chandru@gmail.com>
2017-11-13 10:13:19 +0530

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alerCtrl: AlertController) { }
 
+  doAlert() {
+    let alert = this.alerCtrl.create({
+      title: 'New Friend!',
+      message: 'Your friend, Obi wan Kenobi, just approved your friend request!',
+      buttons: ['Ok']
+    });
+    alert.present();
   }
 
 }
