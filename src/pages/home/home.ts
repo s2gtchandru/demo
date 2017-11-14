c1092f192110fc1d3670d1ab635e75465cb2d7df
Chandrakumar Vijayakumar <s2gt.chandru@gmail.com>
2017-11-14 16:51:22 +0530

    Searchbar Added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,96 +1,41 @@
 import { Component } from '@angular/core';
 //import { NavController } from 'ionic-angular';
 //import { AlertController } from 'ionic-angular';
-import { Platform, ActionSheetController } from 'ionic-angular';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  //constructor(public alerCtrl: AlertController) { }
-
-  constructor(
-    public platform: Platform,
-    public actionsheetCtrl: ActionSheetController
-  ) { }
-  
-  doAlert() {
-    let alert = this.alerCtrl.create({
-      title: 'New Friend!',
-      message: 'Your friend, Obi wan Kenobi, just approved your friend request!',
-      buttons: ['Ok']
-    });
-    alert.present();
-  }
-  
-  doConfirm() {
-    let confirm = this.alerCtrl.create({
-      title: 'Use this lightsaber?',
-      message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
-      buttons: [
-        {
-          text: 'Disagree',
-          handler: () => {
-            console.log('Disagree clicked');
-          }
-        },
-        {
-          text: 'Agree',
-          handler: () => {
-            console.log('Agree clicked');
-          }
-        }
-      ]
-    });
-    confirm.present()
-  }
-  
-  openMenu() {
-    let actionSheet = this.actionsheetCtrl.create({
-      title: 'Albums',
-      cssClass: 'action-sheets-basic-page',
-      buttons: [
-        {
-          text: 'Delete',
-          role: 'destructive',
-          icon: !this.platform.is('ios') ? 'trash' : null,
-          handler: () => {
-            console.log('Delete clicked');
-          }
-        },
-        {
-          text: 'Share',
-          icon: !this.platform.is('ios') ? 'share' : null,
-          handler: () => {
-            console.log('Share clicked');
-          }
-        },
-        {
-          text: 'Play',
-          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
-          handler: () => {
-            console.log('Play clicked');
-          }
-        },
-        {
-          text: 'Favorite',
-          icon: !this.platform.is('ios') ? 'heart-outline' : null,
-          handler: () => {
-            console.log('Favorite clicked');
-          }
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel', // will always sort to be on the bottom
-          icon: !this.platform.is('ios') ? 'close' : null,
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        }
-      ]
-    });
-    actionSheet.present();
+  constructor() { this.initializeItems(); }
+    
+  initializeItems() {
+    this.items = [
+      'Chandru',
+      'Chandraa',
+      'Murali',
+      'Prasanth'
+     
+    ];
   }
 
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+  
+  
+  
+  
 }
